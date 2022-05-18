a2e6c0786fb3e345d53e9f7738dc123064bcadb7
Rufino Martin <rufinomartin77@gmail.com>
2022-05-18 12:02:42 -0300

    Add files via upload
    Queries which conform the latter files for tableau

--- C19 Queries.sql
+++ C19 Queries.sql
@@ -8,47 +8,85 @@ Aggregate Functions, Creating Views, Converting Data Types
 */
 
 
+-- Creation of synonyms of both tables for swifter manipulation --
+
+CREATE SYNONYM Deaths FOR [Covid Analysis].[dbo].[CovidDeaths]
+CREATE SYNONYM Vaccinations FOR [Covid Analysis].[dbo].[CovidVaccinations]
+
 --- FIRST DATASET: CASES & DEATHS ---
 
 select *
-from CovidDeaths$
+FROM Deaths
 order by 3,4;
 
 select *
-from CovidVaccinations$
+from Vaccinations
 order by 1,2;
 
--- 1. Total Cases vs Total Deaths
+-- 1. Total Cases vs Total Deaths  --> tableau
 
 --    > Shows likelihood of dying if you contract covid in your country
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-From CovidDeaths$
+From Deaths
 Where location like '%arg%' --insert any state--
 and continent is not null 
 order by 1,2;
+
+
+-- 1.2 Net Death % --> tableau candidate
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From Deaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- 1.2 Total Death Count --> Tableau
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From Deaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
 
 -- 2. Total Cases vs Population
 
 --    > Shows what percentage of population infected with Covid
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
-From CovidDeaths$
+From Deaths
 Where location like '%arg%'
 order by 1,2;
 
--- 3. Countries with Highest Infection Rate compared to Population
+-- 3. Countries with Highest Infection Rate compared to Population --> Tableau
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 Group by Location, Population
 order by PercentPopulationInfected desc;
 
+-- 3.1 Infected Population + Date --> Tableau
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Deaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
 -- 4. Countries with Highest Death Count per Population
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 Where continent is not null 
 Group by Location
@@ -57,7 +95,7 @@ order by TotalDeathCount desc;
 -- 5. Showing contintents with the highest death count per population
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 Where continent is not null 
 Group by continent
@@ -66,7 +104,7 @@ order by TotalDeathCount desc;
 -- 6. Global Net Numbers
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 where continent is not null 
 --Group By date
@@ -81,12 +119,27 @@ order by 1,2;
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100 -->TO USE LATER
-From CovidDeaths$ dea
-Join CovidVaccinations$ vac
+From Deaths dea
+Join Vaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
 order by 2,3;
+
+-- 7.1 Without new vaccinations --> Tableau Candidate
+
+Select dea.continent, dea.location, dea.date, dea.population
+, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From Deaths dea
+Join Vaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+group by dea.continent, dea.location, dea.date, dea.population
+order by 1,2,3
+
+
 
 -- 8. Using CTE to perform Calculation on Partition By in previous query
 
@@ -96,8 +149,8 @@ as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From CovidDeaths$ dea
-Join CovidVaccinations$ vac
+From Deaths dea
+Join Vaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
@@ -105,6 +158,11 @@ where dea.continent is not null
 )
 Select *, (RollingPeopleVaccinated/Population)*100 as Outcome
 From PopvsVac;
+
+
+
+
+
 
 -- 9. Using Temp Table to perform Calculation on Partition with previous query
 
@@ -122,16 +180,15 @@ New_vaccinations numeric,
 RollingPeopleVaccinated numeric
 );
 
-
 --     > We insert our previous query into that table.
 
 
-Insert into #PercentPopulationVaccinated AS PopulationVaccinated
+Insert into #PercentPopulationVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From CovidDeaths$ dea
-Join CovidVaccinations$ vac
+From Deaths dea
+Join Vaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 --where dea.continent is not null 
@@ -141,45 +198,47 @@ Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated;
 
 
+
+
+
 -- Creating Views to store data for later visualizations --
 
 
 --> 1. At least One Dose: Population vs Vaccination
 
-Create VIEW PercentPopulationVaccinated 
+Create VIEW Percent_Population_Vaccinated AS
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From CovidDeaths$ dea
-Join CovidVaccinations$ vac
+From Deaths dea
+Join Vaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null; 
  
-
 --> 2 . Shows likelihood of dying if you contract covid in your country
 
-Create VIEW as Mortality_Likelihood
+Create VIEW Mortality_Likelihood AS
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-From CovidDeaths$
+From Deaths
 Where location like '%arg%' --insert any state--
 and continent is not null 
 order by 1,2;
 
 --> 3. Countries with Highest Infection Rate compared to Population
 
-Create VIEW as TopInfectedCountries
+Create VIEW Top_Infected_Countries AS
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 Group by Location, Population
 order by PercentPopulationInfected desc;
 
 --> 4. Countries with Highest Death Count per Population
 
-Create VIEW AS NationalMortality
+Create VIEW National_Mortality AS
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 Where continent is not null 
 Group by Location
@@ -187,10 +246,11 @@ order by TotalDeathCount desc;
 
 --> 5. Contintents with the highest death count per population
 
-Create VIEW AS ContinentalMortality
+Create VIEW Continental_Mortality AS
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From CovidDeaths$
+From Deaths
 --Where location like '%arg%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc;
+
